2f1e81f209ed78919a3b27dd75d3cbe09d9f4bb3
jakubrados1 <jakub.rados1@gmail.com>
2025-04-09 12:51:28 +0200

    wip: data structure development

--- main.py
+++ main.py
@@ -15,6 +15,6 @@ def get_user_info(users_data: list)->None:
     for user in users_data:
         print(f'Twój znajomy {user['name']} z miejscowości {user['Location']} opublikował {user['posts']} postów')
 
-print(f'Witaj {users[0]['name']}')
 
+print(f'Witaj {users[0]['name']}')
 get_user_info(users[1:])
