114219b7fe0cc339b1e6bfb5c3658002b62b8c58
jakubrados1 <jakub.rados1@gmail.com>
2025-05-31 15:59:19 +0200

    wip: frontend z interaktywną mapą

--- utils/model.py
+++
@@ -1,7 +0,0 @@
-users:list = [
-    {'name': 'Julia', 'Location': 'Ząbki', 'posts': 10},
-    {'name': 'Julia', 'Location': 'Sokółka', 'posts': 20},
-    {'name': 'Klaudia', 'Location': 'Warszawa', 'posts': 15},
-    {'name': 'Marcin', 'Location': 'Grudziądz', 'posts': 1000},
-    {'name': 'Mateusz', 'Location': 'Lublin', 'posts': 10},
-]
